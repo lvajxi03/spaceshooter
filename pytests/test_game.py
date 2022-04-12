a0e04f20609fc017620faedeb1281dfbbf84c7ec
Marcin Bielewicz <marcin.bielewicz@gmail.com>
2022-04-12 16:44:13 +0200

    Fix pytest invocation

--- pytests/test_game.py
+++ pytests/test_game.py
@@ -6,6 +6,7 @@ Test game module
 
 
 from spaceshooter.game import Game
+from spaceshooter.stypes import Board, Mode
 from unittest.mock import Mock
 
 
@@ -15,7 +16,7 @@ def test_game_1():
     :return: None
     """
     shooter_mock = Mock()
-    shooter_mock.images = {}
+    shooter_mock.images = {'star': None}
     game = Game(shooter_mock)
     game.change_board(Board.GAME)
     assert game.mode == Mode.INIT
